9cabfa17205deb4e6b6ce033cdfb9cbdbfdfab66
王振涛 <766422659@qq.com>
2016-10-25 08:58:25 +0800

    test

--- app/src/main/java/com/example/dllo/carhome/fragment/RecommendFragment.java
+++ app/src/main/java/com/example/dllo/carhome/fragment/RecommendFragment.java
@@ -17,4 +17,5 @@ public class RecommendFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.recommend_fragment,null);
     }
+
 }
